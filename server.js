1851758897fda99ee121937aa6d5e06a63508913
Travis Ripley <theripleys@gmail.com>
2019-05-02 11:50:59 -0700

    updates to server.js index.ejs, contact.ejs

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("views", "./views");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const data = {
+    person: {
+      firstName: "Travez",
+      lastName: "Ripley"
+    }
+  };
+  res.render("index", data);
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+  });
+
+  app.post('/thanks', (req, res) => {
+    res.render('thanks', { contact: req.body })
+  });
+
+app.listen(8080, () => {
+  console.log("listening at http://localhost:8080");
+});
+
+module.exports = app;
