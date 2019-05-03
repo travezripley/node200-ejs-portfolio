f67dbb3bc9ed7fbbdaef97af4f548750c32b6b14
Travis Ripley <theripleys@gmail.com>
2019-05-03 15:11:37 -0700

    adds partials, email

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
+const request = require("request");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
+
+const Mailchimp = require('mailchimp-api-v3');
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY);
+
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/static", express.static("public"));
 
 app.set("views", "./views");
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const data = {
-    person: {
-      firstName: "Travez",
-      lastName: "Scott"
-    }
-  };
-  res.render("index", data);
+  res.render("index");
 });
 
 app.get("/contact", (req, res) => {
@@ -27,10 +31,37 @@ app.get("/contact", (req, res) => {
 });
 
 app.post("/thanks", (req, res) => {
+
+  console.log(req.body);
+
+  const { firstName, lastName, email } = req.body;
+
+  const data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+  }
+  const postData = JSON.stringify(data);
+
+  mailchimp.post('/lists/a06f7fee47/members', data)
+  .then(function(results) {
+  
+  })
+  .catch(function (err) {
+
+  })
+
   res.render("thanks", { contact: req.body });
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("listening at http://localhost:8080");
 });
 
